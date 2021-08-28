493597e1534cd4ce6fd4b941007d572c6b2bc258
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2021-08-29 01:57:14 +0530

    Final

---
+++ src/App.svelte
@@ -0,0 +1,56 @@
+<script>
+	let todos = [];
+	let todo = '';
+	let num = 0;
+	function addTodo() {
+		if (todo) {
+			todos = [
+				... todos,
+				{
+					text: todo,
+					id: num
+				}
+			]
+		}
+		todo = '';
+		num++;
+		console.log(todos)
+	}
+
+	function removeTodo(id) {
+		const number = todos.findIndex(todo => todo.id === id);
+  		todos.splice(number, 1);
+ 		todos = todos;
+	}
+</script>
+
+<main>
+
+	<form on:submit|preventDefault="{addTodo}">
+		<input type="text" placeholder="Add a todo" bind:value={todo}/>
+		<button type="submit">
+			Submit
+		</button>
+	</form>
+
+	<ul>
+		{#each todos as item}
+			<li>
+				{item.text}
+				<button on:click={()=> removeTodo(todo.id)}>
+				</button>
+			</li>
+		{/each}
+	</ul>
+
+</main>
+
+<style>
+main{
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	padding-top: 2em;
+}
+</style>
